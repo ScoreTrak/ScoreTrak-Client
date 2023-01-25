ef4105a0b986b41e16dc55c098f08581575119a9
Ibrahim Diallo <20114089+thisisibrahimd@users.noreply.github.com>
2023-01-25 07:44:04 -0500

    update layouts

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Box, Container } from "@material-ui/core";
-import { MainNav } from "../components/MainNav";
+import { MainNav } from "~/components/MainNav";
+import BaseLayout from "~/layouts/BaseLayout";
 
 export default function DefaultLayout() {
   return (
-    <>
+    <BaseLayout>
       <MainNav />
       <Box m={2} display={"flex"}>
         <Container maxWidth={"xl"}>
           <Outlet />
         </Container>
       </Box>
-    </>
+    </BaseLayout>
   );
 }
